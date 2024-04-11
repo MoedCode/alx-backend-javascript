69ac39238c468909f037a5c6c3d95a1994c2d0ac
MoedCode <mohamed71291@gmail.com>
2024-04-11 22:01:07 +0200

    T0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,12 +1,4 @@
-/**
- * Represents a class room.
- */
 export default class ClassRoom {
-    /**
-     * Creates a new @
-     * @param {Number} maxStudentsSize
-     * students in the class.
-     */
     constructor(maxStudentsSize) {
       this._maxStudentsSize = maxStudentsSize;
     }
