d205b9840f222f0d5ca2cfb51aad1a4b55b50e06
MoedCode <mohamed71291@gmail.com>
2024-04-12 00:03:24 +0200

    Fri Apr 12 00:03:24 EET 2024

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,20 +1,20 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+  constructor (brand, motor, color, range) {
     super(brand, motor, color);
     this.range = range;
   }
 
-  get range() {
+  get range () {
     return this._range;
   }
 
-  set range(value) {
+  set range (value) {
     this._range = value;
   }
 
-  cloneCar() {
+  cloneCar () {
     const Species = super.constructor[Symbol.species];
 
     return new Species();
