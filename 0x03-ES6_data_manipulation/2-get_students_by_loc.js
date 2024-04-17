cb91e71cd6911cfcc38738ee9ccedb66ba287e72
MoedCode <mohamed71291@gmail.com>
2024-04-17 03:36:41 +0200

    0x03-ES6_data_manipulation 2. Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,8 @@
+/*
 export default function getStudentsByLocation(students, city) {
   return students.filter((studentObj) => studentObj.city === city);
+}
+*/
+export default function getStudentsByLocation(StudentsList, concernLocation) {
+  return StudentsList.filter(({ location }) => location === concernLocation);
 }
