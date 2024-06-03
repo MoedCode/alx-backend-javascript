50a0e7ff0d649f43314f2be860ec9567ed0f239a
MoedCode <mohamed71291@gmail.com>
2024-06-03 22:16:43 +0300

    Mon Jun  3 22:16:43 EEST 2024

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const displayMessage = console.log
-module.exports =  displayMessage
-if (require.main === module ){
-    displayMessage("Hello NodeJS!");
+const displayMessage = console.log;
+module.exports = displayMessage;
+if (require.main === module) {
+  displayMessage('Hello NodeJS!');
 }
