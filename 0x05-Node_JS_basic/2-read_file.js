ed35eefd115d12a17f1110ecdecdf007da70cb86
MoedCode <mohamed71291@gmail.com>
2024-06-04 21:37:44 +0300

    Tue Jun  4 21:37:44 EEST 2024

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,7 +2,10 @@
 const fs = require('fs');
 
 const countStudents = (filePath) => {
-  if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(filePath).isFile()) {
     throw new Error('Cannot load the database');
   }
   const fileLines = fs
