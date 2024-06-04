a0c97a2fed1b07a7efa3983f0174f6546f81ef86
MoedCode <mohamed71291@gmail.com>
2024-06-04 03:14:34 +0300

    Tue Jun  4 03:14:34 EEST 2024

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+// Display initial message
+process.stdout.write('Welcome to Holberton School, what is your name?');
+
+// set up event listener for users inputs
+process.stdin.on('readable', () => {
+  // reading
+  const chunk = process.stdin.read();
+  if (chunk) {
+    // now printing so we use stdout
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
