d9f1aa5eef89d1b8d6c2811cc793ed36b048cbb7
MoedCode <mohamed71291@gmail.com>
2024-06-07 04:23:56 +0300

    Fri Jun  7 04:23:56 EEST 2024

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,28 +1,24 @@
 const assert = require('assert');
+const { it, describe } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-  it('should return the sum of two numbers', () => {
-    assert.strictEqual(calculateNumber('SUM', 4, 5), 9);
-    assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
-    assert.strictEqual(calculateNumber('SUM', -4, -5), -9);
+  it('checking if operation is correct 1', () => {
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
   });
-
-  it('should return the difference of two numbers', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 10, 5), 5);
-    assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', -10, -5), -5);
+  it('checking if operation is correct 2', () => {
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
-
-  it('should return the division of two numbers', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 10, 5), 2);
-    assert.strictEqual(calculateNumber('DIVIDE', 10, 0), 'Error');
-    assert.strictEqual(calculateNumber('DIVIDE', -10, -5), 2);
+  it('checking if operation is correct 3', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
-
-  it('should return 0 for unknown operation type', () => {
-    assert.strictEqual(calculateNumber('UNKNOWN', 10, 5), 0);
-    assert.strictEqual(calculateNumber('UNKNOWN', 0, 0), 0);
-    assert.strictEqual(calculateNumber('UNKNOWN', -10, -5), 0);
+  it('checking if operation is correct 4', () => {
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+  it('checking correct type for operation 1', () => {
+    assert.equal(calculateNumber(5, 1, 4), 'Error');
+  });
+  it('checking correct type for operation 2', () => {
+    assert.equal(calculateNumber('plus', 1, 4), 'Error');
   });
 });
